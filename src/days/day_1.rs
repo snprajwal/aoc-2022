32fa201b7188b47755847a0eed166e64e2e99dc8
Prajwal S N <prajwalnadig21@gmail.com>
2022-12-03 23:41:22 +0530

    solve: day 1
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>

--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -1,3 +1,16 @@
 pub fn solve(input: String) -> (String, String) {
-    (String::default(), String::default())
+    let mut sums = input
+        .split("\n\n")
+        .map(|x| {
+            x.split("\n")
+                .flat_map(|v| v.parse::<usize>())
+                .sum::<usize>()
+        })
+        .collect::<Vec<usize>>();
+    sums.sort_by(|x, y| y.cmp(x));
+
+    (
+        sums[0].to_string(),
+        sums.iter().take(3).sum::<usize>().to_string(),
+    )
 }
