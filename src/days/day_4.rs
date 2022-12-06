c4f525a9afd51c13e6f36031be580e45bfde557c
Prajwal S N <prajwalnadig21@gmail.com>
2022-12-07 00:12:11 +0530

    solve: day 4
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>

--- src/days/day_4.rs
+++ src/days/day_4.rs
@@ -1,3 +1,17 @@
 pub fn solve(input: String) -> (String, String) {
-    (String::default(), String::default())
+    let v = input.lines().map(|x| {
+        x.split(",")
+            .flat_map(|y| y.split("-").map(|z| z.parse::<usize>().unwrap()))
+            .collect::<Vec<usize>>()
+    });
+
+    (
+        v.clone()
+            .filter(|x| (x[0] <= x[2] && x[1] >= x[3]) || (x[0] >= x[2] && x[1] <= x[3]))
+            .count()
+            .to_string(),
+        v.filter(|x| (x[0] <= x[2] && x[1] >= x[2]) || (x[2] <= x[0] && x[3] >= x[0]))
+            .count()
+            .to_string(),
+    )
 }
