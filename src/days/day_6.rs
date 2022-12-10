d5026da547f9331067df95c5cf778f88c4fc9ac9
Prajwal S N <prajwalnadig21@gmail.com>
2022-12-10 17:47:38 +0530

    solve: day 6
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>

--- src/days/day_6.rs
+++ src/days/day_6.rs
@@ -1,3 +1,36 @@
 pub fn solve(input: String) -> (String, String) {
-    (String::default(), String::default())
+    let bytes = input.as_bytes();
+    let mut present: [bool; 26] = [false; 26];
+
+    // Part 1
+    let s1 = (0..bytes.len() - 3)
+        .find_map(|i| {
+            for j in i..i + 4 {
+                let k = (bytes[j] - 97) as usize;
+                if present[k] {
+                    present = [false; 26];
+                    return None;
+                }
+                present[k] = true;
+            }
+            Some(i + 4)
+        })
+        .unwrap();
+
+    // Part 2
+    let s2 = (0..bytes.len() - 13)
+        .find_map(|i| {
+            for j in i..i + 14 {
+                let k = (bytes[j] - 97) as usize;
+                if present[k] {
+                    present = [false; 26];
+                    return None;
+                }
+                present[k] = true;
+            }
+            Some(i + 14)
+        })
+        .unwrap();
+
+    (s1.to_string(), s2.to_string())
 }
